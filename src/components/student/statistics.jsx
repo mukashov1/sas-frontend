0d2c40971d7c143c80a3e6733dad1d058ae6fe33
Bakdaulet <bakdaulet.mukashov@gmail.com>
2023-05-09 18:18:33 +0600

    make attendance scrollable

--- src/components/student/statistics.jsx
+++ src/components/student/statistics.jsx
@@ -3,19 +3,31 @@ import { BsFillXCircleFill, BsCheckCircleFill, BsHandIndexThumbFill, BsFillFileE
 import { FaAngleDown, FaParking } from 'react-icons/fa'
 
 const subjects = [
-  { name: 'History', present: 15, absent: 2, 
-  attendance: ['present', 'absent', 'present', 'present', 'absent', 'present', 'present', 'present', 'present', 'present','present', 'present', 'absent','present', 'present', 'absent',], 
-  dates: ['01.03', '02.03', '03.03', '04.03', '05.03', '06.03', '07.03', '08.03', '09.03', '10.03', '11.03', '12.03', '13.03', '14.03', '15.03', '16.03', '17.03']},
-  { name: 'Math', present: 10, absent: 5, attendance: ['present', 'manual', 'present', 'present', 'absent', 'present', 'present', 'present', 'present', 'present'],
-  dates: ['01.03', '02.03', '03.03', '04.03', '05.03', '06.03', '07.03', '08.03', '09.03', '10.03', '11.03', '12.03', '13.03', '14.03', '15.03', '16.03', '17.03'], },
-  { name: 'Programming', present: 1, absent: 3, attendance: ['present', 'absent', 'present', 'present', 'absent', 'present', 'present', 'present', 'present', 'present'],
-  dates: ['01.03', '02.03', '03.03', '04.03', '05.03', '06.03', '07.03', '08.03', '09.03', '10.03', '11.03', '12.03', '13.03', '14.03', '15.03', '16.03', '17.03'], },
-  { name: 'Design Pattern', present: 4, absent: 1, attendance: ['present', 'absent', 'present', 'present', 'absent', 'present', 'present', 'present', 'present', 'present'],
-  dates: ['01.03', '02.03', '03.03', '04.03', '05.03', '06.03', '07.03', '08.03', '09.03', '10.03', '11.03', '12.03', '13.03', '14.03', '15.03', '16.03', '17.03'], },
-  { name: 'DBMS1', present: 9, absent: 2, attendance: ['reason', 'absent', 'present', 'present', 'absent', 'present', 'present', 'present', 'present', 'present'],
-  dates: ['01.03', '02.03', '03.03', '04.03', '05.03', '06.03', '07.03', '08.03', '09.03', '10.03', '11.03', '12.03', '13.03', '14.03', '15.03', '16.03', '17.03'], },
-  { name: 'DBMS2', present: 5, absent: 6, attendance: ['present', 'absent', 'present', 'present', 'absent', 'present', 'present', 'present', 'present', 'present'],
-  dates: ['01.03', '02.03', '03.03', '04.03', '05.03', '06.03', '07.03', '08.03', '09.03', '10.03', '11.03', '12.03', '13.03', '14.03', '15.03', '16.03', '17.03'], },
+  {
+    name: 'History', present: 15, absent: 2,
+    attendance: ['present', 'absent', 'present', 'present', 'absent', 'present', 'present', 'present', 'present', 'present', 'present', 'present', 'absent', 'present', 'present', 'absent',],
+    dates: ['01.03', '02.03', '03.03', '04.03', '05.03', '06.03', '07.03', '08.03', '09.03', '10.03', '11.03', '12.03', '13.03', '14.03', '15.03', '16.03', '17.03']
+  },
+  {
+    name: 'Math', present: 10, absent: 5, attendance: ['present', 'manual', 'present', 'present', 'absent', 'present', 'present', 'present', 'present', 'present'],
+    dates: ['01.03', '02.03', '03.03', '04.03', '05.03', '06.03', '07.03', '08.03', '09.03', '10.03', '11.03', '12.03', '13.03', '14.03', '15.03', '16.03', '17.03'],
+  },
+  {
+    name: 'Programming', present: 1, absent: 3, attendance: ['present', 'absent', 'present', 'present', 'absent', 'present', 'present', 'present', 'present', 'present'],
+    dates: ['01.03', '02.03', '03.03', '04.03', '05.03', '06.03', '07.03', '08.03', '09.03', '10.03', '11.03', '12.03', '13.03', '14.03', '15.03', '16.03', '17.03'],
+  },
+  {
+    name: 'Design Pattern', present: 4, absent: 1, attendance: ['present', 'absent', 'present', 'present', 'absent', 'present', 'present', 'present', 'present', 'present'],
+    dates: ['01.03', '02.03', '03.03', '04.03', '05.03', '06.03', '07.03', '08.03', '09.03', '10.03', '11.03', '12.03', '13.03', '14.03', '15.03', '16.03', '17.03'],
+  },
+  {
+    name: 'DBMS1', present: 9, absent: 2, attendance: ['reason', 'absent', 'present', 'present', 'absent', 'present', 'present', 'present', 'present', 'present'],
+    dates: ['01.03', '02.03', '03.03', '04.03', '05.03', '06.03', '07.03', '08.03', '09.03', '10.03', '11.03', '12.03', '13.03', '14.03', '15.03', '16.03', '17.03'],
+  },
+  {
+    name: 'DBMS2', present: 5, absent: 6, attendance: ['present', 'absent', 'present', 'present', 'absent', 'present', 'present', 'present', 'present', 'present'],
+    dates: ['01.03', '02.03', '03.03', '04.03', '05.03', '06.03', '07.03', '08.03', '09.03', '10.03', '11.03', '12.03', '13.03', '14.03', '15.03', '16.03', '17.03'],
+  },
 ]
 
 // const subjects = JSON.parse(localStorage.getItem('subjects'))
@@ -23,30 +35,30 @@ const subjects = [
 function Dropdown({ subject }) {
   return (
     <div className='dropdown'>
-        <ul>
-          <li>
-            <strong>Date</strong>
-            {subject.dates.map((date, index) => (
-              <span key={date}>{date}</span>
-            ))}
-          </li>
-          <li>
-            <strong>Attendance</strong>
-            {subject.dates.map((date, index) => (
-              <span key={date}>
-                {subject.attendance[index] === 'manual' ? (
-                  <i style={{ color: 'green' }}><BsHandIndexThumbFill /></i>
-                ) : subject.attendance[index] === 'present' ? (
-                  <i style={{ color: 'green' }}><BsCheckCircleFill /></i>
-                ) : subject.attendance[index] === 'reason' ? (
-                  <i style={{ color: 'blue' }}><FaParking /></i>
-                ) : (
-                  <i style={{ color: 'red' }}><BsFillXCircleFill /></i>
-                )}
-              </span>
-            ))}
-          </li>
-        </ul>
+      <ul>
+        <li>
+          <strong>Date</strong>
+          {subject.dates.map((date, index) => (
+            <span key={date}>{date}</span>
+          ))}
+        </li>
+        <li>
+          <strong>Attendance</strong>
+          {subject.dates.map((date, index) => (
+            <span key={date}>
+              {subject.attendance[index] === 'manual' ? (
+                <i style={{ color: 'green' }}><BsHandIndexThumbFill /></i>
+              ) : subject.attendance[index] === 'present' ? (
+                <i style={{ color: 'green' }}><BsCheckCircleFill /></i>
+              ) : subject.attendance[index] === 'reason' ? (
+                <i style={{ color: 'blue' }}><FaParking /></i>
+              ) : (
+                <i style={{ color: 'red' }}><BsFillXCircleFill /></i>
+              )}
+            </span>
+          ))}
+        </li>
+      </ul>
     </div>
   );
 }
@@ -55,7 +67,7 @@ function Dropdown({ subject }) {
 export default function Statistics() {
   const [showDropdown, setShowDropdown] = useState(true)
   const [selectedSubject, setSelectedSubject] = useState(subjects[0])
-  
+
   const handleDropdownClick = (subject) => {
     if (selectedSubject === subject) {
       setSelectedSubject(null)
@@ -65,7 +77,7 @@ export default function Statistics() {
       setShowDropdown(true)
     }
   }
-  
+
 
   const handleDropdownClose = () => {
     setSelectedSubject(null)
